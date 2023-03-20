ed97cb85424cc423d0528f132153dd263a4bfd8a
August Damiani <acdamiani@outlook.com>
2023-03-20 19:00:25 +0000

    update loader animation

--- src/lib/Loader.svelte
+++ src/lib/Loader.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="flex gap-2">
+<!-- <div class="flex gap-2">
   <div class="w-3 h-3 rounded-full bg-zinc-500 animate-pulse" />
   <div
     class="w-3 h-3 rounded-full bg-zinc-500 animate-pulse animation-delay-[150ms]"
@@ -9,4 +9,103 @@
   <div
     class="w-3 h-3 rounded-full bg-zinc-500 animate-pulse animation-delay-[300ms]"
   />
-</div>
+</div> -->
+
+<svg
+  width="45"
+  height="46.7"
+  viewBox="0 0 135 140"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="currentColor"
+>
+  <rect y="10" width="15" height="120" rx="6">
+    <animate
+      attributeName="height"
+      begin="0.5s"
+      dur="1s"
+      values="120;110;100;90;80;70;60;50;40;140;120"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+    <animate
+      attributeName="y"
+      begin="0.5s"
+      dur="1s"
+      values="10;15;20;25;30;35;40;45;50;0;10"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+  </rect>
+  <rect x="30" y="10" width="15" height="120" rx="6">
+    <animate
+      attributeName="height"
+      begin="0.25s"
+      dur="1s"
+      values="120;110;100;90;80;70;60;50;40;140;120"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+    <animate
+      attributeName="y"
+      begin="0.25s"
+      dur="1s"
+      values="10;15;20;25;30;35;40;45;50;0;10"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+  </rect>
+  <rect x="60" width="15" height="140" rx="6">
+    <animate
+      attributeName="height"
+      begin="0s"
+      dur="1s"
+      values="120;110;100;90;80;70;60;50;40;140;120"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+    <animate
+      attributeName="y"
+      begin="0s"
+      dur="1s"
+      values="10;15;20;25;30;35;40;45;50;0;10"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+  </rect>
+  <rect x="90" y="10" width="15" height="120" rx="6">
+    <animate
+      attributeName="height"
+      begin="0.25s"
+      dur="1s"
+      values="120;110;100;90;80;70;60;50;40;140;120"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+    <animate
+      attributeName="y"
+      begin="0.25s"
+      dur="1s"
+      values="10;15;20;25;30;35;40;45;50;0;10"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+  </rect>
+  <rect x="120" y="10" width="15" height="120" rx="6">
+    <animate
+      attributeName="height"
+      begin="0.5s"
+      dur="1s"
+      values="120;110;100;90;80;70;60;50;40;140;120"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+    <animate
+      attributeName="y"
+      begin="0.5s"
+      dur="1s"
+      values="10;15;20;25;30;35;40;45;50;0;10"
+      calcMode="linear"
+      repeatCount="indefinite"
+    />
+  </rect>
+</svg>
