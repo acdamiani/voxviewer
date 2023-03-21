97c112a3bd96ac385af1b3fe8f01dfaeec9628be
August Damiani <acdamiani@outlook.com>
2023-03-20 22:10:12 -0500

    fix file not committed

--- rs/src/buf.rs
+++ rs/src/buf.rs
@@ -1,4 +1,3 @@
-use js_sys::{Float32Array, Function};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,17 +8,10 @@ pub struct WasmSampleBuffer {
 #[wasm_bindgen]
 impl WasmSampleBuffer {
     #[wasm_bindgen(constructor)]
-    pub fn new(buffer_length: u32, f: &Function) -> Self {
-        let size = buffer_length as usize;
-        let mut buffer = vec![0.0; size];
-
-        unsafe {
-            let array = Float32Array::view_mut_raw(buffer.as_mut_ptr(), size);
-            f.call1(&JsValue::NULL, &JsValue::from(array))
-                .expect("Callback function should not throw");
+    pub fn new(raw_samples: Vec<f32>) -> Self {
+        Self {
+            buffer: raw_samples,
         }
-
-        Self { buffer }
     }
 }
 
