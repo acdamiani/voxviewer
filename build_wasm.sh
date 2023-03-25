37074ab972d0f63e28a340cabdd160e1a76f9676
August Damiani <acdamiani@outlook.com>
2023-03-24 19:07:57 -0500

    fixes rustwasm/wasm-pack#1039

---
+++ build_wasm.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+if !command -v js &> /dev/null
+then
+  echo "jq command not found"
+  exit 127
+fi
+
+if !command -v wasm-pack &> /dev/null
+then
+  echo "wasm-pack command not found"
+  exit 127
+fi
+
+if [ ! -d "./rs" ]
+then
+  echo "could not find crate \"rs\""
+  exit 2
+fi
+
+cd rs
+wasm-pack build --target web
+
+tmp=$(mktemp)
+
+cd pkg
+jq '. += { type: "module" }' package.json > "$tmp" && mv "$tmp" package.json
