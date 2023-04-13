e2c3b7cd412c8ba77e52f7483b5de052b84aa894
August Damiani <acdamiani@outlook.com>
2023-04-13 15:39:08 +0000

    reload shell

--- build_prod.sh
+++ build_prod.sh
@@ -2,6 +2,8 @@
 
 yum install jq -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 cd rs
