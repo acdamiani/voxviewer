75967789c51f064f55fe595a8f1ae2508c974056
August Damiani <acdamiani@outlook.com>
2023-04-13 15:34:56 +0000

    redirect arg

--- build_prod.sh
+++ build_prod.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 yum install jq -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 cd rs
